89c0af5861ac41a86b3586c108ae4425b13cd62c
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-01 06:03:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  return string.toLowerCase("hello"); string.toUpperCase("HELLO") string ("I love you")
+  return string.toLowerCase("hello"); string.toUpperCase("HELLO") string ("I love you, Grandma")
 }
