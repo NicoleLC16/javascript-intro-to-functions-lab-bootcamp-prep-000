31a1f1448e516ba8a83f833a26d77f3624e8ed77
Nicole Cabral <asalwaysnicole@outlook.com>
2019-10-01 06:26:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string){
   else(string=string.toUpperCase())
   console.log ("YES INDEED!");
   
-  else(string="I love you, Grandma")
+  else if(string="I love you, Grandma")
   console.log ("I love you, too")
 }
